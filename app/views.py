e01d538026d17c6018a55833955742affa123f1d
birkan <b.561birkan@gmail.com>
2022-08-29 13:41:52 +0300

    Read data in template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def index(request):
     return render(request, "app/index.html")
+
+# read process
+
+def student_list(request):
+     students = Student.objects.all()
+     context = {
+         "students":students
+     }
+     return render(request, "app/student_list.html",context)
