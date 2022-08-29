765913b71a03e5b1645682f027849a9395d83e02
birkan <b.561birkan@gmail.com>
2022-08-29 13:22:13 +0300

    learnt templates logic and using admin panel

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1>hello</h1>")
+def index(request):
+    return render(request, "app/index.html")
